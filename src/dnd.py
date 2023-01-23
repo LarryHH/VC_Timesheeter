9f57dc7666ce5dc7c4636416e7f9af095facd80e
Larry <larryhieuhuynh@gmail.com>
2023-01-23 23:28:54 +0800

    update version

--- src/dnd.py
+++ src/dnd.py
@@ -11,7 +11,7 @@ from form import WindowForm
 import webbrowser
 import requests
 
-CURRENT_VERSION = "v1.2.0"
+CURRENT_VERSION = "v1.2.1"
 
 if getattr(sys, 'frozen', False):
     application_path = sys._MEIPASS
